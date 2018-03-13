b5b1a1709423ea1786ec3c4d713dcb735f36feb5
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-03-13 22:43:17 +0200

    UI sketching.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+        <div className="track">
+          <div className="train"></div>
+        </div>
     );
   }
 }
