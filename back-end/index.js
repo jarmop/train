9af87dc1761325599d99be716aa63a60a307323c
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-03-13 23:45:18 +0200

    Created back end app for more convenient data crunching.

---
+++ back-end/index.js
@@ -0,0 +1,26 @@
+const geolib = require('geolib');
+
+let train1 = {
+  lat: 60.17171747413841,
+  lon: 24.942053947481092,
+  delta: 17,
+  direction: 0,
+  status: 1,
+  action: 'deleted',
+};
+
+let train2 = {
+  lat: 60.30315743603303,
+  lon: 25.04952444568184,
+  delta: -38,
+  direction: 0,
+  status: 1,
+  action: 'modified',
+};
+
+let distance = geolib.getDistance(
+    {latitude: train1.lat, longitude: train1.lon},
+    {latitude: train2.lat, longitude: train2.lon},
+);
+
+console.log(distance);
