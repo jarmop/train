0359d9705ae8c49ade9e77ef6212c65b9b32cd90
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-03-17 11:50:50 +0200

    Add sections.json containing sections that are close to keharata.

--- back-end/data.js
+++ back-end/data.js
@@ -4,46 +4,129 @@ const jsonfile = require('jsonfile');
 const URL_TRACK_SECTIONS = 'https://rata.digitraffic.fi/api/v1/metadata/track-sections';
 const URL_TRAIN_SCHEDULE = 'https://rata.digitraffic.fi/api/v1/trains/latest/8761';
 // const URL_TRAIN = 'https://rata.digitraffic.fi/api/v1/trains/2018-03-16/8761';
-const KEHARATA_TRACK_NUMBER = '123';
-const KEHARATA_FILENAME = 'keharata.json';
+const KEHARATA_1_TRACK_NUMBER = '001';
+const KEHARATA_1_MAX_LENGTH = 7;
+const KEHARATA_3_TRACK_NUMBER = '003';
+const KEHARATA_3_MAX_LENGTH = 20;
+const KEHARATA_123_TRACK_NUMBER = '123';
+const KEHARATA_FILENAME = 'keharata[TRACK_NUMBER].json';
 
-const getSectionLocationMetres = (section) => {
-  return section.ranges[0].startLocation.kilometres * 1000 +
-      section.ranges[0].startLocation.metres;
-};
+// Only stoppping points here. (Missing "gohst stations".)
+const keharataStationsTrack1 = ['HKI', 'PSL', 'ILA', 'HPL'];
+const keharataStationsTrack3 = [
+  'HKI',
+  'PSL',
+  'KÄP',
+  'OLK',
+  'PMK',
+  'ML',
+  'TNA',
+  'PLA',
+  'TKL',
+  'HKH',
+];
+const keharataStationsTrack123 = [
+  'POH',
+  'KAN',
+  'MLO',
+  'MYR',
+  'LOH',
+  'MRL',
+  'LAV',
+  'VKS',
+  'VEH',
+  'KTÖ',
+  'AVP',
+  'LEN',
+  'ASO',
+  'LNÄ',
+];
 
-const getSectionLength = (section) => {
-  let {startLocation, endLocation} = section.ranges[0];
-  return (endLocation.kilometres * 1000 + endLocation.metres) -
-      (startLocation.kilometres * 1000 + startLocation.metres);
-};
+// const getSectionLocationMetres = (section) => {
+//   return section.ranges[0].startLocation.kilometres * 1000 +
+//       section.ranges[0].startLocation.metres;
+// };
+//
+// const getSectionLength = (section) => {
+//   let {startLocation, endLocation} = section.ranges[0];
+//   return (endLocation.kilometres * 1000 + endLocation.metres) -
+//       (startLocation.kilometres * 1000 + startLocation.metres);
+// };
 
 const removeDuplicates = (sections) => {
   let hashTable = {};
-  // let mapLocationToSection = {};
   for (section of sections) {
-    hashTable[section.trackSectionCode + getSectionLocationMetres(section)] = section;
-    // mapLocationToSection[getSectionLocationMetres(section)] = section;
+    let trackAndCode = section.station + '-' + section.trackSectionCode;
+    if (hashTable.hasOwnProperty(trackAndCode)) {
+      console.log(trackAndCode);
+    } else {
+      hashTable[trackAndCode] = section;
+    }
   }
   return Object.values(hashTable);
-  // return Object.values(mapLocationToSection);
+};
+
+const filterKeharataSections = (sections) => {
+  return sections.filter(section => {
+    let isPartOfKeharata = false;
+    for (range of section.ranges) {
+      if (
+          (
+              [range.startLocation.track, range.endLocation.track]
+                  .includes(KEHARATA_1_TRACK_NUMBER)
+              &&
+              range.endLocation.kilometres < KEHARATA_1_MAX_LENGTH
+          )
+          ||
+          (
+              [range.startLocation.track, range.endLocation.track]
+                  .includes(KEHARATA_3_TRACK_NUMBER)
+              &&
+              range.endLocation.kilometres < KEHARATA_3_MAX_LENGTH
+          )
+          ||
+          (
+              [range.startLocation.track, range.endLocation.track]
+                  .includes(KEHARATA_123_TRACK_NUMBER)
+          )
+      ) {
+        isPartOfKeharata = true;
+        break;
+      }
+    }
+    return isPartOfKeharata;
+  });
 };
 
 const filterSections = (sections) => {
   console.log('filterSections');
-  filteredSections = sections
-      .filter(section => (
-          section.ranges[0].startLocation.track === KEHARATA_TRACK_NUMBER
-          ||
-          section.ranges[0].endLocation.track === KEHARATA_TRACK_NUMBER
-          ||
-          ['HKI', 'PSL', 'KÄP', 'OLK', 'PMK', 'ML', 'TNA', 'PLA', 'TKL', 'HKH', 'ILA', 'HPL'].includes(section.station)
-      ))
-      .filter(section => getSectionLength(section) > 0);
+  console.log('total sections: ' + sections.length);
+  filteredSections = filterKeharataSections(sections);
 
-  // filteredSections = removeDuplicates(filteredSections);
+  // .filter(section => getSectionLength(section) > 0);
 
+  filteredSections = removeDuplicates(filteredSections);
+
+  // let multiRangeSections = 0;
+  // for (section of filteredSections) {
+  //   if (section.ranges.length > 1) {
+  //     console.log(section.station + '-' + section.trackSectionCode);
+  //     multiRangeSections++;
+  //   }
+  // }
+  // let hashTable = {};
+  // for (section of filteredSections) {
+  //   hashTable[section.station] = 1;
+  // }
+
+  // 1 - 206 / 207
+  // 3 - 328 / 328
+  // 123 - 129 / 132
+  // console.log(multiRangeSections);
   console.log(filteredSections.length);
+  // console.log(Object.keys(hashTable).length);
+  // console.log(Object.keys(hashTable));
+  // console.log(filteredSections);
 
   return filteredSections;
 
@@ -60,12 +143,14 @@ const fetchTrackSections = () => {
 exports.initializeKeharata = () => {
   fetchTrackSections()
       .then(sections => filterSections(sections))
-      .then(sections => jsonfile.writeFile(KEHARATA_FILENAME, sections, (error) => {
-        if (error) {
-          console.log(error);
-        }
-        console.log(KEHARATA_FILENAME + ' saved!');
-      }))
+      .then(sections => jsonfile.writeFile('data/sections.json', sections,
+          (error) => {
+            if (error) {
+              console.log(error);
+            }
+            console.log(KEHARATA_FILENAME + ' saved!');
+          },
+      ));
 };
 
 exports.getKeharataTrackSections = () => {
@@ -75,6 +160,6 @@ exports.getKeharataTrackSections = () => {
         reject(error);
       }
       resolve(data);
-    })
+    });
   });
 };
