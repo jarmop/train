4b147d6447c8d847b035a3c7961c13d5465abed6
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-03-20 19:19:20 +0200

    Fetch locations simultaneously from digi.traffic and junatkartalla API for comparison.

--- front-end/src/Tracker.js
+++ front-end/src/Tracker.js
@@ -29,7 +29,19 @@ class Tracker extends Component {
     super(props);
 
     this.state = {
-      trainNumber: (new URL(window.location.href)).pathname.split('/').slice(-1).pop(),
+      trainNumber: (new URL(window.location.href)).pathname.split('/')
+          .slice(-1)
+          .pop(),
+      data1: {
+        longitude: 0,
+        latitude: 0,
+        speed: 0,
+      },
+      data2: {
+        longitude: 0,
+        latitude: 0,
+        speed: 0,
+      }
     };
   }
 
@@ -43,10 +55,27 @@ class Tracker extends Component {
   updateLocation() {
     service.getLocation(this.state.trainNumber)
         .then(({longitude, latitude, speed}) => {
+          console.log('update 1');
           this.setState({
-            longitude,
-            latitude,
-            speed,
+            data1: {
+              longitude,
+              latitude,
+              speed,
+            }
+          });
+        })
+        .catch(message => {
+          console.log(message);
+        });
+    service.getLocation2(this.state.trainNumber)
+        .then(({longitude, latitude, speed}) => {
+          console.log('update 2');
+          this.setState({
+            data2: {
+              longitude,
+              latitude,
+              speed,
+            }
           });
         })
         .catch(message => {
@@ -55,32 +84,41 @@ class Tracker extends Component {
   }
 
   componentDidMount() {
-    service.getStations().then(stations => this.setState({stations}))
+    service.getStations().then(stations => this.setState({stations}));
     this.updateLocation();
     this.poll();
   }
 
   render() {
-    let {longitude, latitude, speed, stations} = this.state;
+    let {data1, data2, stations} = this.state;
 
     if (!stations) {
       return '';
     }
 
-    let x = mapLonToMap(longitude);
-    let y = mapLatToMap(latitude);
+    let x1 = mapLonToMap(data1.longitude);
+    let y1 = mapLatToMap(data1.latitude);
+    let x2 = mapLonToMap(data2.longitude);
+    let y2 = mapLatToMap(data2.latitude);
 
     return (
         <div className="tracker">
           <div>
-            Coordinates: {longitude + ', ' +latitude}
+            Coordinates1: {data1.longitude + ', ' + data1.latitude}
+            <br/>
+            Speed1: {data1.speed}
           </div>
           <div>
-            Speed: {speed}
+            Coordinates2: {data2.longitude + ', ' + data2.latitude}
+            <br/>
+            Speed2: {data2.speed}
+          </div>
+          <div>
           </div>
           <div className="tracker-map" style={{width: width, height: height}}>
             {stations.map(station =>
                 <div
+                    key={station.stationShortCode}
                     className="tracker-map__station"
                     style={{
                       left: mapLonToMap(station.longitude) + 'px',
@@ -88,9 +126,16 @@ class Tracker extends Component {
                     }}
                 >
                   {station.stationName}
-                  </div>
+                </div>
             )}
-            <div className="tracker-map__train" style={{left: x + 'px', bottom: y + 'px'}}></div>
+            <div
+                className="tracker-map__train tracker-map__train--1"
+                style={{left: x1 + 'px', bottom: y1 + 'px'}}
+            ></div>
+            <div
+                className="tracker-map__train tracker-map__train--2"
+                style={{left: x2 + 'px', bottom: y2 + 'px'}}
+            ></div>
           </div>
         </div>
     );
--- front-end/src/TrackerService.js
+++ front-end/src/TrackerService.js
@@ -3,7 +3,20 @@ import stoppingSections from './data/stopping-sections';
 let keharataStationCodes = Object.keys(stoppingSections);
 
 const URL_LOCATION = 'https://rata.digitraffic.fi/api/v1/train-locations/latest/[TRAIN_NUMBER]';
+const URL_LOCATION2 = 'https://junatkartalla-cal-prod.herokuapp.com/trains/[PREVIOUS_RESPONSE_TIMESTAMP]';
 const URL_STATIONS = 'https://rata.digitraffic.fi/api/v1/metadata/stations';
+
+let previousResponseTimestamp = 1;
+
+export const getStations = () => {
+  return fetch(URL_STATIONS)
+      .then(response => response.json())
+      .then(stations => {
+        let keharataStations = stations.filter(station => keharataStationCodes.includes(station.stationShortCode));
+        return keharataStations;
+      })
+      .catch();
+};
 
 export const getLocation = (train) => {
   return fetch(URL_LOCATION.replace(/\[TRAIN_NUMBER\]/, train))
@@ -19,13 +32,25 @@ export const getLocation = (train) => {
       .catch();
 };
 
-export const getStations = () => {
-  return fetch(URL_STATIONS)
-      .then(response => response.json())
-      .then(stations => {
-        let keharataStations = stations.filter(station => keharataStationCodes.includes(station.stationShortCode));
-        console.log(keharataStations);
-        return keharataStations;
-      })
-      .catch();
+export const getLocation2 = (train) => {
+  return new Promise((resolve, reject) => {
+    fetch(URL_LOCATION2.replace(/\[PREVIOUS_RESPONSE_TIMESTAMP\]/, previousResponseTimestamp))
+        .then(response => response.json())
+        .then(json => {
+          previousResponseTimestamp = json.responseTimestamp;
+          if (json.trains.hasOwnProperty(train)) {
+            let trainData = json.trains[train];
+            resolve({
+              longitude: trainData.longitude,
+              latitude: trainData.latitude,
+              speed: trainData.speed,
+            });
+          } else {
+            reject('not updated');
+          }
+        })
+        .catch(message => {
+          reject(message);
+        });
+  });
 };
