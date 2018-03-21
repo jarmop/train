49b6367f5b5af3c0e82994ac1def3fddafd4f03a
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-03-21 20:21:42 +0200

    Copy ApiRace from Tracker. Remove some old prototypes.

---
+++ front-end/src/ApiRace.js
@@ -0,0 +1,162 @@
+import React, {Component} from 'react';
+import * as service from './TrackerService';
+import './Tracker.css';
+
+let minLon = 24.84;
+let maxLon = 25.10;
+let minLat = 60.16;
+let maxLat = 60.33;
+
+let latRatio = 4000;
+let lonRatio = 2300;
+
+let width = (maxLon - minLon) * lonRatio;
+let height = (maxLat - minLat) * latRatio;
+
+// console.log(width);
+// console.log(height);
+
+let mapLonToMap = (lon) => {
+  return Math.floor((lon - minLon) * lonRatio);
+};
+
+let mapLatToMap = (lat) => {
+  return height - Math.floor((lat - minLat) * latRatio);
+};
+
+class ApiRace extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      trainNumber: (new URL(window.location.href)).pathname.split('/')
+          .slice(-1)
+          .pop(),
+      data1: {
+        longitude: 0,
+        latitude: 0,
+        speed: 0,
+      },
+      data2: {
+        longitude: 0,
+        latitude: 0,
+        speed: 0,
+      }
+    };
+  }
+
+  poll() {
+    setTimeout(() => {
+      this.updateLocation();
+      this.poll();
+    }, 5000);
+  }
+
+  updateLocation() {
+    service.getLocation(this.state.trainNumber)
+        .then(({longitude, latitude, speed}) => {
+          console.log('update 1');
+          this.setState({
+            data1: {
+              longitude,
+              latitude,
+              speed,
+            }
+          });
+        })
+        .catch(message => {
+          console.log(message);
+        });
+    service.getLocation2(this.state.trainNumber)
+        .then(({longitude, latitude, speed}) => {
+          console.log('update 2');
+          this.setState({
+            data2: {
+              longitude,
+              latitude,
+              speed,
+            }
+          });
+        })
+        .catch(message => {
+          console.log(message);
+        });
+  }
+
+  componentDidMount() {
+    service.getStations().then(stations => this.setState({stations}));
+    this.updateLocation();
+    this.poll();
+  }
+
+  render() {
+    let {data1, data2, stations} = this.state;
+
+    if (!stations) {
+      return '';
+    }
+
+    let x1 = mapLonToMap(data1.longitude);
+    let y1 = mapLatToMap(data1.latitude);
+    let x2 = mapLonToMap(data2.longitude);
+    let y2 = mapLatToMap(data2.latitude);
+
+    return (
+        <div className="tracker">
+          <div>
+            Coordinates1: {data1.longitude + ', ' + data1.latitude}
+            <br/>
+            Speed1: {data1.speed}
+          </div>
+          <div>
+            Coordinates2: {data2.longitude + ', ' + data2.latitude}
+            <br/>
+            Speed2: {data2.speed}
+          </div>
+          <svg width={width} height={height}>
+            {stations.map(station =>
+                <Station
+                    key={station.stationShortCode}
+                    x={mapLonToMap(station.longitude)}
+                    y={mapLatToMap(station.latitude)}
+                    name={station.stationName}
+                />
+            )}
+            <Train x={x1} y={y1} color={'green'}/>
+            <Train x={x2} y={y2} color={'blue'}/>
+          </svg>
+        </div>
+    );
+  }
+}
+
+let Station = ({x, y, name}) => {
+  let radius = 6;
+  let strokeWidth = 1;
+  return (
+      <svg>
+        <circle
+            cx={x}
+            cy={y}
+            r={radius - strokeWidth}
+            style={{
+              fill: 'red', stroke: 'black', strokeWidth: strokeWidth,
+            }}
+        />
+        <text x={x + radius} y={y + radius} fill="black">{name}</text>
+      </svg>
+  );
+};
+
+let Train = ({x, y, color}) => (
+    <circle
+        cx={x}
+        cy={y}
+        r={5}
+        style={{
+          fill: color, stroke: 'black', strokeWidth: 1,
+        }}
+    />
+);
+
+export default ApiRace;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,15 +2,9 @@ import React, {Component} from 'react';
 import 'font-awesome/css/font-awesome.css';
 import './App.css';
 import {
-  addLeadingZero, getSection, getSectionIds, getSectionLength,
-  getSectionStartLocation,
-  getSectionEndLocation, getSectionTrack, getTrainTrackingData, formatSectionId,
-  getLogStartTime, updateOccupied, getInitialOccupied, updateOccupiedReverse,
-  getOccupied,
+  addLeadingZero,
 } from './service';
-import Map from './Map';
-import Detail from './Detail';
-import Tracker from './Tracker';
+import ApiRace from './ApiRace';
 
 const formatDate = (timestamp) => {
   let date = new Date(timestamp);
@@ -21,105 +15,19 @@ const formatDate = (timestamp) => {
       + addLeadingZero(date.getSeconds());
 };
 
-const parseUsefulSectionData = (section) => {
-  return {
-    startLocation: getSectionStartLocation(section),
-    endLocation: getSectionEndLocation(section),
-    length: getSectionLength(section),
-  }
-};
-
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      sections: [],
-      occupied: [],
-      log: [],
-      timeSpeedMs: 10000,
-      trainNumber: 9154,
-    };
   }
 
   componentDidMount() {
-    // let sections = getSectionIds()
-        // .filter(sectionId =>
-        //     ['001', '123'].includes(getSectionTrack(getSection(sectionId)))
-            // ['003'].includes(getSectionTrack(getSection(sectionId)))
-            // &&
-            // getSectionStartLocation(getSection(sectionId)) > 0
-            // &&
-            // getSectionEndLocation(getSection(sectionId)) < 20000
-        // )
-        // .map(sectionId => ({
-        //     id: sectionId,
-        //     ...parseUsefulSectionData(getSection(sectionId)
-        // )}));
-        // .sort((sectionA, sectionB) =>
-        //     sectionB.startLocation - sectionA.startLocation
-        // );
-
-    // let sections = [];
-
-    // console.log(sections);
-
-    // let log = getTrainTrackingData().sort((a, b) => a.id - b.id);
-
-    // console.log(log[0]);
-
-    // this.setState({
-    //   sections: sections,
-    //   log: log,
-    //   // logPointer: 0,
-    //   date: getLogStartTime(log),
-    //   // date: new Date(0),
-    //   // occupied: getInitialOccupied(log),
-    //   occupied: getOccupied(this.state.trainNumber),
-    // });
-  }
-
-  goBack(speed) {
-    let previousDate = this.state.date;
-    let newDate = new Date(previousDate.getTime() - this.state.timeSpeedMs * speed);
-    let occupied = updateOccupiedReverse(
-        this.state.log, this.state.occupied, previousDate, newDate);
-    this.setState({
-      date: newDate,
-      occupied: occupied,
-    });
-  }
-
-  goForward(speed) {
-    let previousDate = this.state.date;
-    let newDate = new Date(previousDate.getTime() + this.state.timeSpeedMs * speed);
-    let occupied = updateOccupied(
-        this.state.log, this.state.occupied, previousDate, newDate);
-    this.setState({
-      date: newDate,
-      occupied: occupied,
-    });
   }
 
   render() {
-    let {sections, occupied, date} = this.state;
-    // let track = (
-    //     <div className="track">
-    //       <div className="train"></div>
-    //     </div>
-    // );
-
     return (
         <div className="container">
-          {/*<Control*/}
-              {/*date={date}*/}
-              {/*onUp={(speed) => this.goBack(speed)}*/}
-              {/*onDown={(speed) => this.goForward(speed)}*/}
-          {/*/>*/}
-          <Detail/>
-          <Tracker/>
-          {/*<Map sections={sections} occupied={occupied}/>*/}
-          {/*<Table sections={sections}/>*/}
+          <ApiRace/>
         </div>
     );
   }
@@ -127,43 +35,8 @@ class App extends Component {
 
 export default App;
 
-// let Table = ({sections}) => (
-//     <table border="1" style={{borderCollapse: 'collapse'}}>
-//       <thead>
-//       <tr>
-//         <th>id</th>
-//         <th>start</th>
-//         <th>end</th>
-//         <th>length</th>
-//       </tr>
-//       </thead>
-//       <tbody>
-//       {sections.map(section =>
-//           <tr key={section.id}>
-//             <td>{section.id}</td>
-//             <td>{section.startLocation}</td>
-//             <td>{section.endLocation}</td>
-//             <td>{section.length}</td>
-//           </tr>
-//       )}
-//       </tbody>
-//     </table>
-// );
-
-let Control = ({onUp, onDown, date}) => (
-    <div className="control">
-      <div>{formatDate(date)}</div>
-      <button className="control__button" onClick={() => onUp(10)}>
-        <i className="fa fa-angle-double-up"></i>
-      </button>
-      <button className="control__button" onClick={() => onUp(1)}>
-        <i className="fa fa-angle-up"></i>
-      </button>
-      <button className="control__button" onClick={() => onDown(1)}>
-        <i className="fa fa-angle-down"></i>
-      </button>
-      <button className="control__button" onClick={() => onDown(10)}>
-        <i className="fa fa-angle-double-down"></i>
-      </button>
+let Track = () => (
+    <div className="track">
+      <div className="train"></div>
     </div>
 );
