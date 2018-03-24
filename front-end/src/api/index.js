e4ed71cf4ebda829bb96b24799b4642ea8b3a4e9
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-03-24 15:17:41 +0200

    Remove queryString module. (Fails to minify: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#npm-run-build-fails-to-minify)

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,5 +1,3 @@
-import queryString from 'query-string';
-
 // const URL_STATION = 'https://rata.digitraffic.fi/api/v1/live-trains/station/[STATION_SHORT_CODE]?minutes_after_departure=60&minutes_before_departure=60';
 const URL_STATION = 'https://rata.digitraffic.fi/api/v1/live-trains/station/[STATION_SHORT_CODE]' +
     '?minutes_before_arrival=0' +
