ac0557c081749f635e0816631aa04529542f15f0
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-03-01 22:45:47 +0200

    Updates

---
+++ precache-manifest.c3c3a08104411e2a26514bc99931cf1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6fadbf88411bed629ded76f9207125",
+    "url": "/train/index.html"
+  },
+  {
+    "revision": "b0e070b730912e91411b",
+    "url": "/train/static/css/2.dde651e0.chunk.css"
+  },
+  {
+    "revision": "0ddc322f60c95f0cc7c4",
+    "url": "/train/static/css/main.75441c4f.chunk.css"
+  },
+  {
+    "revision": "b0e070b730912e91411b",
+    "url": "/train/static/js/2.8cbcf387.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/train/static/js/2.8cbcf387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ddc322f60c95f0cc7c4",
+    "url": "/train/static/js/main.1faa126c.chunk.js"
+  },
+  {
+    "revision": "7dc21c8b08bb8693562f",
+    "url": "/train/static/js/runtime-main.0da6fdf3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/train/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/train/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/train/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/train/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/train/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
